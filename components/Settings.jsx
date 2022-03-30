16b43e050aad8f1a984080bd214cc9de103f817b
bonnie <arlolocos@gmail.com>
2022-03-30 03:30:52 -0400

    created Settings component

---
+++ components/Settings.jsx
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Settings() {
+  return (
+    <View style={styles.settings}>
+
+    </View>
+  );
+
+  const styles = StyleSheet.create({
+    settings: {
+      // width: 80,
+      // height: 50,
+      // borderColor: '#0a0a0a',
+      // borderWidth: 2,
+      // padding: 5,
+      // borderRadius: 4,
+      // marginTop: '2%',
+      // marginRight: '40%',
+      // backgroundColor: '#fff',
+      // alignItems: 'center',
+      // justifyContent: 'center',
+    },
+    text: {
+      // color: 'rgba(50, 5, 255, 1)',
+    }
+}
