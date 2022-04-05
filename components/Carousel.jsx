8869f662e40356b0d515d7c00a2ade93411229f2
Nathan Lucy <cosmocoder1@gmail.com>
2022-04-04 23:11:29 -0500

    basic carousel functionality complete.  Styling + refinements in progress

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import VideoCard from './VideoCard.jsx';
+import arrow from '../assets/Arrow.png';
+
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 var previousResults = null;
 
@@ -8,6 +18,27 @@ var Carousel = (props) => {
   var searchResults = props.searchResults;
   var trending = props.trending;
   var newResults = false;
+  var carousel = true;
+
+  // var swiper = $('.swiper-container').swiper({
+  //   onSlideChangeStart: function(){
+  //     // on the first slide
+  //     if (swiper.activeIndex==0) {
+  //       $('.left-arrow').hide()
+  //       $('.right-arrow').show()
+  //     }
+  //     // most right postion
+  //     else if (swiper.activeIndex == swiper.slides.length-1) {
+  //       $('.left-arrow').show()
+  //       $('.right-arrow').hide()
+  //     }
+  //     // middle positions
+  //     else {
+  //       $('.left-arrow').show()
+  //       $('.right-arrow').show()
+  //     }
+  //   }
+  // })
 
   if (JSON.stringify(searchResults) !== JSON.stringify(previousResults)) {
     previousResults = searchResults;
@@ -26,10 +57,36 @@ var Carousel = (props) => {
         <VideoCard program={item} />)
       }</div>
     )
+  } else if (!carousel) {
+    return (
+      <div>carousel under construction</div>
+    )
   } else {
     return (
-      <div id="carousel">
-        <div>loading carousel content...</div>
+      <div id="carouselBox">
+        <img src={arrow} className="swiper-button-prev"></img>
+        <div id="swiperBox">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            speed={800}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev'
+            }}
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={((swiper) => console.log(swiper))}
+          >
+            <SwiperSlide className="videoCard">Video Card 1</SwiperSlide>
+            <SwiperSlide className="videoCard">Video Card 2</SwiperSlide>
+            <SwiperSlide className="videoCard">Video Card 3</SwiperSlide>
+            <SwiperSlide className="videoCard">Video Card 4</SwiperSlide>
+            <SwiperSlide className="videoCard">Video Card 5</SwiperSlide>
+          </Swiper>
+        </div>
+        <img src={arrow} className="swiper-button-next"></img>
       </div>
     )
   }
