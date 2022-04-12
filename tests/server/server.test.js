254a65306738524f466ede1787d8399f377e96ab
Nathan Lucy <cosmocoder1@gmail.com>
2022-04-12 12:33:17 -0500

    merge

--- tests/server/server.test.js
+++ tests/server/server.test.js
@@ -3,7 +3,6 @@ const expect  = require('chai').expect;
 const app = require('../../server/app.js');
 const request = supertest(app);
 
-
 it('Server startup successful', (done) => {
     // const res = await request.get('/test');
     // expect(response.status).toBe(200);
